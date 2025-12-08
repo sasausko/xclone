9e15d2ebf0f6fc9aeaee2e10e35e0a0ba433570f
sasausko <sadosadoushko@gmail.com>
2025-12-08 15:35:30 +0100

    Add and set ActionText and ActiveStorage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_rich_text :body
+  has_many_attached :images
 
   validates :title, presence: true
   validates :body, presence: true
   validates :user, presence: true
-  
+
   accepts_nested_attributes_for :user
 end
