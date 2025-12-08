6b723846cedfa87a1ea617c1ee41f51e485dbeb5
sasausko <sadosadoushko@gmail.com>
2025-12-08 04:34:13 +0100

    Add Pages and fix styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+  get "pages/terms"
+  get "pages/privacy"
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "posts#index"
   resources :posts
-
 end
