28bf51f1db4a4d127ab3db128aa6c15a51f53c17
sasausko <sadosadoushko@gmail.com>
2025-12-18 15:37:04 +0100

    Add nickname to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
+# app/models/user.rb
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :email, presence: true, uniqueness: true
-  has_many :posts, dependent: :destroy
+
+  validates :nickname,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 50 }
+
+  # Віртуальний атрибут для входу
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+
+    # Отримуємо значення з параметра login (якщо він є)
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where([
+        "lower(email) = :value OR lower(nickname) = :value",
+        { value: login.downcase }
+      ]).first
+    elsif conditions.has_key?(:email) || conditions.has_key?(:nickname)
+      where(conditions.to_h).first
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_08_141514) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_13_150237) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -62,11 +62,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_08_141514) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
